66e7d34480d6b141f52e007378fa2fb34165e2d6
LucasDibz <lucas.f.dib@gmail.com>
2022-01-26 13:03:49 +0000

    fix: naming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,7 +56,7 @@ const HomePage: NextPage = () => {
           backgroundSize: 'cover',
           backgroundBlendMode: 'multiply',
           transition: '4s linear',
-          animation: 'Hue-rotate 10s infinite linear',
+          animation: 'color-rotate 10s infinite linear',
         }}
       >
         <Box
